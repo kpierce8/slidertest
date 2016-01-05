da4f3f4bb34d7794b0f5c11273dad2e5e126917d
Ken Pierce <kpierce8@gmail.com>
2016-01-04 21:46:38 -0800

    initial commit with two working labels

--- sliderTest/ViewController.swift
+++ sliderTest/ViewController.swift
@@ -2,6 +2,11 @@
 //  ViewController.swift
 //  sliderTest
 //
+//  This example is to figure out how to dynamically use a slider to
+//    set values in data fields. Probably need to try a double slider some time for
+//    setting precision values. Can probably use a range switch statement with text
+//    values to make a pseudo drop-down.
+//
 //  Created by Ken Pierce on 12/28/15.
 //  Copyright © 2015 Ken Pierce. All rights reserved.
 //
@@ -13,6 +18,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +28,48 @@ class ViewController: UIViewController {
     }
 
 
+   
+    
+    var activeTextBox = 1
+    var activeLabel: UILabel?
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var mySlider: UISlider!
+    
+    @IBAction func setTextOneActive(sender: AnyObject) {
+        activeTextBox = 1
+        mySlider.value = 0.5
+        sliderMessage(mySlider)
+    }
+
+
+    
+    @IBAction func setTextTwoActive(sender: AnyObject) {
+        activeTextBox = 2
+        mySlider.value = 0.5
+        sliderMessage(mySlider)
+    }
+    
+    
+    @IBAction func sliderMessage(sender: AnyObject) {
+        switch  activeTextBox {
+        case 1:
+            let maxVal = Float(1000.0)
+            label1.text = String(format: "%.3f", self.mySlider.value * maxVal)
+            
+        case 2:
+            label2.text = String(format: "%.2f", self.mySlider.value)
+        default:
+            label1.text = String(format: "%.2f", self.mySlider.value)
+        }
+       
+        
+
+    }
+    
 }
 
